5f2573767aca168c24b4416371d597aac3d07de8
Yauheni Charniauski <Yauheni.Charniauski@itechart-group.com>
2021-03-10 17:11:03 +0300

    check lint script

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 import ReactDom from "react-dom";
+import "./style/styles.css";
 
 function HelloWorld(props) {
   return <h1>Hello world, {props.name}</h1>;
@@ -8,7 +9,7 @@ function HelloWorld(props) {
 
 function App() {
   return (
-    <div>
+    <div className="header">
       <HelloWorld name="Yauheni" />
     </div>
   );
