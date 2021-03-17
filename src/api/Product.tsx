8c760aee20ef298bb4738f15a5e590f2af4e6c68
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-03-17 20:36:38 +0300

    Classes. Added Product class.

---
+++ src/api/Product.tsx
@@ -0,0 +1,10 @@
+export class Product {
+  name: string;
+  price: Number;
+  age: Number;
+  category: string;
+  platform: string[];
+  shortdescription: string;
+  image: string;
+  date: string;
+}
