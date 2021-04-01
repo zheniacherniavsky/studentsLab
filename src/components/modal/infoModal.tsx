f29c6b28bd450fa80082fe2196bef6db266ae185
Yauheni <zheniacherniavskyplay@gmail.com>
2021-04-02 01:16:20 +0300

    InfoModal refactoring.

--- src/components/modal/infoModal.tsx
+++ src/components/modal/infoModal.tsx
@@ -1,14 +1,49 @@
 import Modal from "@/elements/modal";
 
-// TODO: components for modals
+export const enum InfoType {
+  ALERT,
+  PROMPT,
+}
 
 export interface InfoModalProps {
   infoModalHeader: string;
   infoModalText: string;
-  infoModalType: string;
+  infoModalType: InfoType;
   infoModalCallback: () => void;
   closeInfoModalCallback: () => void;
 }
+
+interface IButton {
+  text: string;
+  callback?: () => void;
+  close: () => void;
+}
+
+const Button = (p: IButton) => (
+  <button
+    type="button"
+    className="modal_button"
+    onClick={() => {
+      if (p.callback) p.callback();
+      p.close();
+    }}
+  >
+    {p.text}
+  </button>
+);
+
+const AlertButtons = ({ close }: { close: () => void }) => (
+  <div className="buttons_container">
+    <Button text="Ok" close={close} />
+  </div>
+);
+
+const PromptButtons = ({ callback, close }: { callback: () => void; close: () => void }) => (
+  <div className="buttons_container">
+    <Button text="Yes" close={close} callback={callback} />
+    <Button text="Cancel" close={close} />
+  </div>
+);
 
 export function InfoModal({
   infoModalHeader,
@@ -22,39 +57,9 @@ export function InfoModal({
       <div className="container">
         <h2>{infoModalHeader}</h2>
         <p>{infoModalText}</p>
-        {infoModalType === "alert" ? (
-          <button
-            type="button"
-            className="modal_button"
-            onClick={() => {
-              closeInfoModalCallback();
-            }}
-          >
-            Ok
-          </button>
-        ) : null}
-        {infoModalType === "prompt" ? (
-          <div className="buttons_container">
-            <button
-              type="button"
-              className="modal_button"
-              onClick={() => {
-                if (infoModalCallback) infoModalCallback();
-                closeInfoModalCallback();
-              }}
-            >
-              Yes
-            </button>
-            <button
-              type="button"
-              className="modal_button"
-              onClick={() => {
-                closeInfoModalCallback();
-              }}
-            >
-              Cancel
-            </button>
-          </div>
+        {infoModalType === InfoType.ALERT ? <AlertButtons close={closeInfoModalCallback} /> : null}
+        {infoModalType === InfoType.PROMPT ? (
+          <PromptButtons callback={infoModalCallback} close={closeInfoModalCallback} />
         ) : null}
       </div>
     </Modal>
