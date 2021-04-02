d8d51030bae3fb6c8dc04abe58a3d12f67be23ca
Yauheni <zheniacherniavskyplay@gmail.com>
2021-04-02 21:22:00 +0300

    Profile page. Added user interface.

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,14 +1,52 @@
 import useTypedSelector from "@/hooks/useTypedSelector";
 import "./profilePage.scss";
+import noPhotoImage from "@/assets/images/no-photo.png";
+import { useState } from "react";
+import TextInput from "@/elements/inputs/textInput";
+import Textarea from "@/elements/inputs/textarea";
 
 const ProfilePage = () => {
   const { username } = useTypedSelector((state) => state.user);
+  const [newUsername, setNewUsername] = useState("");
+  const [newUsernameError, setNewUsernameError] = useState("");
+
+  const [description, setDescription] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
+
   return (
     <div className="profilepage page_content_container">
       <h2>{username} profile page</h2>
       <div className="information">
-        <div>image</div>
-        <form />
+        <div className="avatar">
+          <img src={noPhotoImage} alt="Empty profile" />
+          <button type="button" onClick={() => alert("This feature is currently disabled.")}>
+            Change profile image
+          </button>
+        </div>
+        <div className="data">
+          <TextInput
+            label="Username"
+            id="login"
+            value={newUsername}
+            maxLength={30}
+            handleChange={setNewUsername}
+            errorDispatch={setNewUsernameError}
+          />
+          <span className="error">{newUsernameError}</span>
+          <Textarea
+            label="Profile description"
+            id="description"
+            value={description}
+            maxLength={200}
+            handleChange={setDescription}
+            errorDispatch={setDescriptionError}
+          />
+          <span className="error">{descriptionError}</span>
+        </div>
+        <div className="buttons">
+          <button type="button">Save profile</button>
+          <button type="button">Change password</button>
+        </div>
       </div>
     </div>
   );
