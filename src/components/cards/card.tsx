11dafb8b0a4254ee17ab4e890e0da9c708ccfb0e
Yauheni <zheniacherniavskyplay@gmail.com>
2021-04-06 00:48:54 +0300

    Card. Added rating. +Categories links fixes at home page.

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,23 +1,31 @@
 import "./card.scss";
 import IProduct from "@/api/product";
 
-const Card = ({ product: p }: { product: IProduct }) => (
-  <div className="card_container">
-    <div className="front">
-      <img src={p.image} alt="Product" />
-      <div>
-        <span className="title">{p.name}</span>
-        <span className="ageLimit">{p.price} RUB</span>
+const Card = ({ product: p }: { product: IProduct }) => {
+  const rating = [];
+  for (let i = 1; i <= 5; i++) {
+    rating.push(i <= p.rating ? <span className="active" /> : <span />);
+  }
+
+  return (
+    <div className="card_container">
+      <div className="front">
+        <img src={p.image} alt="Product" />
+        <div>
+          <span className="title">{p.name}</span>
+          <span className="ageLimit">{p.price} RUB</span>
+        </div>
+        <div className="rating">{rating}</div>
+      </div>
+      <div className="back">
+        <p>{p.shortdescription}</p>
+        <div>
+          <span>{p.age}+</span>
+          <button type="button">Show game</button>
+        </div>
       </div>
     </div>
-    <div className="back">
-      <p>{p.shortdescription}</p>
-      <div>
-        <span>{p.age}+</span>
-        <button type="button">Show game</button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Card;
